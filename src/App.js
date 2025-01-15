1dcbee902cfa290fb1553da5b5e233586bab03bd
Neeharika Srini <neeharika.srinivasan@gmail.com>
2025-01-15 23:33:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Zero2Infynite Website
+    </div>
+  );
+}
+
+export default App;
